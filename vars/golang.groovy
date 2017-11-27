96280cfc89d91b7dd1d68741e3b173fd9710ccfb
Victor Bjelkholm <victorbjelkholm@gmail.com>
2017-11-27 01:43:08 +0100

    Add tests for windows

--- vars/golang.groovy
+++ vars/golang.groovy
@@ -1,6 +1,26 @@
 def call() {
   stage('tests') {
     parallel(
+      windows: {
+        node(label: 'windows') {
+          ansiColor('xterm') {
+            def root = tool name: '1.9.2', type: 'go'
+            def jobNameArr = "${JOB_NAME}"
+            def jobName = jobNameArr.split("/")[0..1].join("\\\\")
+            def originalWs = "${WORKSPACE}"
+            ws("${originalWs}\\src\\github.com\\${jobName}") {
+              withEnv(["GOROOT=${root}", "GOPATH=${originalWs}", "PATH=$PATH;${root}\\bin;${originalWs}\\bin"]) {
+                bat 'go get -v github.com/whyrusleeping/gx'
+                bat 'go get -v github.com/whyrusleeping/gx-go'
+                checkout scm
+                bat 'gx --verbose install --global'
+                bat 'gx-go rewrite'
+                bat "go test -v ./..."
+              }
+            }
+          }
+        }
+      },
       linux: {
         node(label: 'linux') {
           ansiColor('xterm') {
@@ -9,8 +29,7 @@ def call() {
             def jobName = jobNameArr.split("/")[0..1].join("/")
             def originalWs = "${WORKSPACE}"
             ws("${originalWs}/src/github.com/${jobName}") {
-              withEnv(["GOROOT=${root}", "GOPATH=${originalWs}/", "PATH+GO=${root}/bin"]) {
-                env.PATH="${GOPATH}/bin:$PATH"
+              withEnv(["GOROOT=${root}", "GOPATH=${originalWs}", "PATH=$PATH:${root}/bin:${originalWs}/bin"]) {
                 sh 'go get -v github.com/whyrusleeping/gx'
                 sh 'go get -v github.com/whyrusleeping/gx-go'
                 checkout scm
@@ -30,8 +49,7 @@ def call() {
             def jobName = jobNameArr.split("/")[0..1].join("/")
             def originalWs = "${WORKSPACE}"
             ws("${originalWs}/src/github.com/${jobName}") {
-              withEnv(["GOROOT=${root}", "GOPATH=${originalWs}/", "PATH+GO=${root}/bin"]) {
-                env.PATH="${GOPATH}/bin:$PATH"
+              withEnv(["GOROOT=${root}", "GOPATH=${originalWs}", "PATH=$PATH:${root}/bin:${originalWs}/bin"]) {
                 sh 'go get -v github.com/whyrusleeping/gx'
                 sh 'go get -v github.com/whyrusleeping/gx-go'
                 checkout scm
