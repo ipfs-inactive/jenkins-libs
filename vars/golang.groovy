00a7622a6a3076d192f1c6543551e2aec20174fb
Victor Bjelkholm <victorbjelkholm@gmail.com>
2018-01-23 14:51:58 +0100

    Enable windows again, show output after tests run

--- vars/golang.groovy
+++ vars/golang.groovy
@@ -20,35 +20,35 @@ def call(opts = []) {
   def test = defVal(opts['test'], defaultTest)
   stage('tests') {
     parallel(
-      // windows: {
-      //   node(label: 'windows') {
-      //     ansiColor('xterm') {
-      //       def root = tool name: '1.9.2', type: 'go'
-      //       def jobNameArr = "${JOB_NAME}"
-      //       def jobName = jobNameArr.split("/")[0..1].join("\\\\").toLowerCase()
-      //       def originalWs = "${WORKSPACE}"
-      //       ws("${originalWs}\\src\\github.com\\${jobName}") {
-      //         def goEnv = ["GOROOT=${root}", "GOPATH=${originalWs}", "PATH=$PATH;${root}\\bin;${originalWs}\\bin"]
-      //         withEnv(goEnv + env) {
-      //           bat 'go get -v github.com/whyrusleeping/gx'
-      //           bat 'go get -v github.com/whyrusleeping/gx-go'
-      //           bat 'go get -v github.com/jstemmer/go-junit-report'
-      //           checkout scm
-      //           bat 'gx --verbose install --global'
-      //           bat 'gx-go rewrite'
-      //           try {
-      //             bat test + ' > output | tail -f output'
-      //             bat 'go-junit-report > junit-report-windows.xml'
-      //           } catch (err) {
-      //             throw err
-      //           } finally {
-      //             junit allowEmptyResults: true, testResults: 'junit-report-*.xml'
-      //           }
-      //         }
-      //       }
-      //     }
-      //   }
-      // },
+      windows: {
+        node(label: 'windows') {
+          ansiColor('xterm') {
+            def root = tool name: '1.9.2', type: 'go'
+            def jobNameArr = "${JOB_NAME}"
+            def jobName = jobNameArr.split("/")[0..1].join("\\\\").toLowerCase()
+            def originalWs = "${WORKSPACE}"
+            ws("${originalWs}\\src\\github.com\\${jobName}") {
+              def goEnv = ["GOROOT=${root}", "GOPATH=${originalWs}", "PATH=$PATH;${root}\\bin;${originalWs}\\bin"]
+              withEnv(goEnv + env) {
+                bat 'go get -v github.com/whyrusleeping/gx'
+                bat 'go get -v github.com/whyrusleeping/gx-go'
+                bat 'go get -v github.com/jstemmer/go-junit-report'
+                checkout scm
+                bat 'gx --verbose install --global'
+                bat 'gx-go rewrite'
+                try {
+                  bat test + ' > output & type output'
+                  bat 'type output | go-junit-report > junit-report-windows.xml'
+                } catch (err) {
+                  throw err
+                } finally {
+                  junit allowEmptyResults: true, testResults: 'junit-report-*.xml'
+                }
+              }
+            }
+          }
+        }
+      },
       linux: {
         node(label: 'linux') {
           ansiColor('xterm') {
@@ -66,7 +66,7 @@ def call(opts = []) {
                 sh 'gx --verbose install --global'
                 sh 'gx-go rewrite'
                 try {
-                  sh test + ' > output | tail -f output'
+                  sh test + ' 2>&1 | tee output'
                   sh 'cat output | go-junit-report > junit-report-linux.xml'
                 } catch (err) {
                   throw err
@@ -95,7 +95,7 @@ def call(opts = []) {
                 sh 'gx --verbose install --global'
                 sh 'gx-go rewrite'
                 try {
-                  sh test + ' > output | tail -f output'
+                  sh test + ' 2>&1 | tee output'
                   sh 'cat output | go-junit-report > junit-report-macos.xml'
                 } catch (err) {
                   throw err
