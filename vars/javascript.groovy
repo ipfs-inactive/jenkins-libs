c2eddd7212b4b7c0524fb13f9edf5de6a58989cb
victorbjelkholm <victorbjelkholm@gmail.com>
2018-02-28 13:08:00 +0100

    Parallize aegir scripts

--- vars/javascript.groovy
+++ vars/javascript.groovy
@@ -16,13 +16,13 @@ import groovy.transform.Field
 @Field final String yarnPath = './node_modules/.bin/yarn'
 
 // Step for running tests on a specific nodejs version with windows
-def windowsStep (version) {
+def windowsBuildStep (version) {
   node(label: 'windows') { ansiColor('xterm') { withEnv(['CI=true']) {
     // need to make sure we're using the right line endings
     bat 'git config --global core.autocrlf input'
     checkout scm
     fileExists 'package.json'
-    nodejs('9.2.0') {
+    nodejs(version) {
       // delete node_modules if it's there
       bat 'del /s /q node_modules >nul 2>&1'
       // install local version of yarn (prevent concurrency issues again)
@@ -32,20 +32,13 @@ def windowsStep (version) {
       // install dependencies with a mutex lock
       bat yarnPath + ' --mutex network'
       // run actual tests
-      try {
-        bat yarnPath + ' test'
-      } catch (err) {
-        throw err
-      } finally {
-        junit allowEmptyResults: true, testResults: 'junit-report-*.xml'
-        cleanWs()
-      }
+      stash(name: 'windows'+version)
     }
   }}}
 }
 
 // Step for running tests on a specific nodejs version with unix compatible OS
-def unixStep(version, nodeLabel) {
+def unixBuildStep(version, nodeLabel) {
   node(label: nodeLabel) { ansiColor('xterm') { withEnv(['CI=true']) {
     checkout scm
     fileExists 'package.json'
@@ -53,51 +46,105 @@ def unixStep(version, nodeLabel) {
       sh 'rm -rf node_modules/'
       sh 'npm install yarn@' + yarnVersion
       sh yarnPath + ' --mutex network'
+      stash(name: nodeLabel+version)
+    }
+  }}}
+}
+
+def windowsTestStep(cmd, version) {
+  node(label: 'windows') { ansiColor('xterm') { withEnv(['CI=true']) {
+    unstash(name: 'windows'+version)
+    nodejs(version) {
       try {
-        if (nodeLabel == 'linux') { // if it's linux, we need xvfb for display emulation (chrome)
-          wrap([$class: 'Xvfb', parallelBuild: true, autoDisplayName: true]) {
-            sh yarnPath + ' test'
-          }
-        } else {
-          sh yarnPath + ' test'
-        }
+        bat cmd
       } catch (err) {
         throw err
       } finally {
-        junit allowEmptyResults: true, testResults: 'junit-report-*.xml'
-        cleanWs()
+        junit allowEmptyResults: false, testResults: 'junit-report-*.xml'
       }
     }
   }}}
 }
 
+def unixTestStep(cmd, version, nodeLabel) {
+  node(label: nodeLabel) { ansiColor('xterm') { withEnv(['CI=true']) {
+    unstash(name: nodeLabel+version)
+    nodejs(version) {
+      try {
+        if (nodeLabel == 'linux') { // if it's linux, we need xvfb for display emulation (chrome)
+          wrap([$class: 'Xvfb', parallelBuild: true, autoDisplayName: true]) {
+            sh cmd
+          }
+        } else {
+          sh cmd
+        }
+      } catch (err) {
+        throw err
+      } finally {
+        junit allowEmptyResults: false, testResults: 'junit-report-*.xml'
+      }
+    }
+  }}}
+}
+
+
 // Helper function for getting the right platform + version
-def getStep(os, version) {
+def getBuildStep(os, version) {
   return {
     if (os == 'macos' || os == 'linux') {
-      return unixStep(version, os)
+      return unixBuildStep(version, os)
     }
     if (os == 'windows') {
-      return windowsStep(version)
+      return windowsBuildStep(version)
+    }
+  }
+}
+
+def getTestStep(cmd, os, version) {
+  return {
+    if (os == 'macos' || os == 'linux') {
+      return unixTestStep(cmd, version, os)
+    }
+    if (os == 'windows') {
+      return windowsTestStep(cmd, version)
     }
   }
 }
 
 
 def call() {
- stage('Tests') {
+ def hasAegirScripts = true
+ stage('Build') {
   // Create map for all the os+version combinations
-  def steps = [:]
+  def buildSteps = [:]
   for (os in osToTests) {
       for (nodejsVersion in nodejsVersionsToTest) {
           def stepName = os + ' - ' + nodejsVersion
-          steps[(stepName)] = getStep(os, nodejsVersion)
+          buildSteps[(stepName)] = getBuildStep(os, nodejsVersion)
       }
   }
-  // Maximum runtime: 1 hour
   timeout(time: 1, unit: 'HOURS') {
-    // execute those steps in parallel
-    parallel steps
+    parallel buildSteps
+  }
+ }
+ stage('Test') {
+  // check if test:* exists and add `npm run test:*`
+  // if no test:* was found, add `npm run test`
+  def testSteps = [:]
+  for (os in osToTests) {
+      for (nodejsVersion in nodejsVersionsToTest) {
+          def stepName = os + ' - ' + nodejsVersion
+          if (hasAegirScripts) {
+            testSteps[(stepName + " test:node")]    = getTestStep("npm run test:node", os, nodejsVersion)
+            testSteps[(stepName + " test:browser")] = getTestStep("npm run test:browser", os, nodejsVersion)
+            // testSteps[(stepName)] = getTestStep("npm test:webworker", os, nodejsVersion)
+          } else {
+            testSteps[(stepName)] = getTestStep("npm run test", os, nodejsVersion)
+          }
+      }
+  }
+  timeout(time: 1, unit: 'HOURS') {
+    parallel testSteps
   }
  }
 }
